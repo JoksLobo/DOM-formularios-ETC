dacfe2f61f60fd9b671fecf81af4bc8e2b993978
JoksLobo <jocson_herry@hotmail.com>
2026-01-09 15:05:37 -0300

    exemplos de eventos html

---
+++ eventos-html/script.js
@@ -0,0 +1,11 @@
+function onClick() {
+  console.log("Clicando no botão");
+}
+
+function onMouseOver() {
+  console.log("Passando o mouse pelo texto");
+}
+
+function onKeyPress() {
+  console.log("escrevendo no input");
+}
