9aa821683cf83ef8963f1026557100f3f20d3285
JoksLobo <jocson_herry@hotmail.com>
2026-01-15 16:16:43 -0300

    manipulando elementos html com as funções criarElementoInnerHTML e criarElementoTextContent

--- criando-elementos-js/scripts.js
+++ criando-elementos-js/scripts.js
@@ -1,9 +1,25 @@
-function criarElemento() {
+function criarElementoInnerText() {
+  const novoInput = document.createElement("input");
   const ul = document.getElementById("listaElementos");
+  const novaLi = document.createElement("li");
+  novaLi.innerText = "Novo input com Inner Text";
 
+  novaLi.appendChild(novoInput);
+  ul.appendChild(novaLi);
+}
+
+function criarElementoInnerHTML() {
+  const ul = document.getElementById("listaElementos");
+  const novaLi = document.createElement("li");
+  novaLi.innerHTML = "<p>Criei um texto</P>";
+  ul.appendChild(novaLi);
+}
+function criarElementoTextContent() {
+  const ul = document.getElementById("listaElementos");
   const novaLi = document.createElement("li");
   const novoInput = document.createElement("input");
 
+  novaLi.textContent = "Atribuindo conteúdo TextContent";
   novaLi.appendChild(novoInput);
   ul.appendChild(novaLi);
 }
