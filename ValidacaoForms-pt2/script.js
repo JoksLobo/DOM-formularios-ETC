559ccef29ca45fdbccc241e04361f29537b6843c
JoksLobo <jocson_herry@hotmail.com>
2026-01-23 16:23:37 -0300

    Validação de Campos em Javascript 3

--- ValidacaoForms-pt2/script.js
+++ ValidacaoForms-pt2/script.js
@@ -6,20 +6,35 @@ document.addEventListener("DOMContentLoaded", function () {
       e.preventDefault();
     }
 
-    function validarCamposObrigatorios() {
-      let camposObrigatorios = document.querySelectorAll(".obrigatorio");
-
-      let camposValidos = true;
-
-      for (let i = 0; camposObrigatorios.length > i; i++) {
-        let campo = camposObrigatorios[i];
-
-        if (campo.value === "" || campo.value === null) {
-          console.log("Este campo é obrigatorio");
-          camposValidos = false;
-        }
-      }
-      return camposValidos;
+    if (!compararValores()) {
+      e.preventDefault();
     }
   });
+
+  function validarCamposObrigatorios() {
+    let camposObrigatorios = document.querySelectorAll(".obrigatorio");
+
+    let camposValidos = true;
+
+    for (let i = 0; camposObrigatorios.length > i; i++) {
+      let campo = camposObrigatorios[i];
+
+      if (campo.value === "" || campo.value === null) {
+        console.log("Este campo é obrigatorio");
+        camposValidos = false;
+      }
+    }
+    return camposValidos;
+  }
+
+  function compararValores() {
+    let senha = document.getElementById("senha");
+    let confirmarSenha = document.getElementById("confirmarSenha");
+
+    if (senha.value !== confirmarSenha.value) {
+      console.log("As senhas não são iguais");
+      return false;
+    }
+    return true;
+  }
 });
